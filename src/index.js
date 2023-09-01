e466bb7eb3e724401e3fb8da8d3b21d6450ca547
Skanda Koushik <93520290+SkandaKoushik@users.noreply.github.com>
2023-09-01 20:05:56 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EmojiCodeApp from './EmojiCodeApp';
+import './index.css'; // You may need to import global styles here
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EmojiCodeApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
